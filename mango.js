5c6dafb36000791be7dfe2e96516114465914185
PulakJain77 <69963406+PulakJain77@users.noreply.github.com>
2020-10-06 13:41:33 +0530

    Add files via upload

---
+++ mango.js
@@ -0,0 +1,17 @@
+class Mango{
+ constructor(){
+     var options={
+     isStatic:true,
+     restitution:0,
+    friction:1
+     }
+ }
+
+ mango = loadImage('sprites/mango.png');
+ World.add(world,mango)
+
+display(){
+    
+}
+
+}
